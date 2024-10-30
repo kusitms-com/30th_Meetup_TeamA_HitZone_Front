d157008d20d8d672d28d6fdb08215a488e1f9c0c
2unhi <ironhak1106@gmail.com>
2024-10-30 20:09:51 +0900

    (#2) 🎨 design: 구역 가이드, 야구 문화, 마이페이지 기본 레이아웃 구현

---
+++ src/pages/Culture/Culture.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "../../components/layout/Header";
+import NavBar from "../../components/layout/NavBar";
+
+const Culture = () => {
+  return (
+    <div>
+      <Header />
+      <main className="p-4">
+        <h1>야구 문화</h1>
+      </main>
+      <NavBar />
+    </div>
+  );
+};
+
+export default Culture;
