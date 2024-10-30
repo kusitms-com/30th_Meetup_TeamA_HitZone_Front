dbfd2ebc61b3fdf391ce153d81ff7ccb2a852344
inaemon <github32377@gmail.com>
2024-10-30 22:15:53 +0900

    (#4) 🎨 design: 첫 시작 로딩화면 구현

--- src/pages/Onboarding/components/InitPage.tsx
+++ src/pages/Onboarding/components/InitPage.tsx
@@ -1,10 +1,12 @@
+import Image from 'next/image';
+import logo from '../../../assets/svg/hitzone_logo.svg';
+
 const Page = () => {
     return (
-        <div>
-            <h1>안녕</h1>
+        <div className="flex justify-center items-center w-full h-screen bg-main-20">
+            <Image src={logo} alt="logo" width={258} height={45} />
         </div>
     )
 }
-
 
 export default Page
