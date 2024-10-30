d16c700fdd1b1841328da0e19f30ddb0c8e52725
inaemon <github32377@gmail.com>
2024-10-30 22:14:45 +0900

    (#4) 🎨 design: 온보딩 동작 로직 구현

--- src/pages/Onboarding/Onboarding.tsx
+++ src/pages/Onboarding/Onboarding.tsx
@@ -1,10 +1,52 @@
-const Onboarding = () => {
-    return (
-        <div>
-            <h1>온보딩</h1>
-        </div>
-    )
-}
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
 
+import InitPage from "./components/InitPage"
+import LoginPage from "./components/LoginPage"
+import SignupPage from "./components/SignupPage"
+
+const Onboarding = () => {
+    // 로딩 상태 관리
+    const [loading, setLoading] = useState(true);
+    // 로그인 상태 관리
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    // 처음/기존 사용자 상태 관리
+    const [isFirstTimeUser, setIsFirstTimeUser] = useState(true);
+    // useRouter 훅 사용
+    const router = useRouter();
+
+    // 로딩 상태 시뮬레이션
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setLoading(false);
+            // 여기서 백엔드랑 API 통신하면서 로그인 상태와 최초 사용자 여부를 설정하기
+            // 예를 들어:
+            // setIsLoggedIn(true); // 로그인 상태 업데이트
+            // setIsFirstTimeUser(false); // 기존 사용자로 업데이트
+
+        }, 2000); // 2초 후 로딩 종료
+
+        return () => clearTimeout(timer);
+    }, []);
+
+    // 로딩 상태시 렌더링
+    if (loading) {
+        return <InitPage />;
+    }
+
+    // 로그아웃 상태시 렌더링
+    if (!isLoggedIn) {
+        return <LoginPage />;
+    }
+
+    // 로그인 및 처음 사용자 상태시 렌더링
+    if (isFirstTimeUser) {
+        return <SignupPage />;
+    }
+
+    // 로그인 및 기존 사용자 상태시 렌더링하지 않고 /main으로 리디렉션
+    router.push('/main'); // /main으로 이동
+    return null; // 리디렉션 중일 때 아무것도 렌더링하지 않음
+}
 
 export default Onboarding
