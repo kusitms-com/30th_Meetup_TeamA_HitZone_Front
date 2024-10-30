092b8191758b4a10e0ad27007139d680d9f8695d
2unhi <ironhak1106@gmail.com>
2024-10-30 19:02:50 +0900

    (#1) 🎨 design: 공통 Header 디자인

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,9 +1,38 @@
+import React from "react";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import HitzoneLogo from "../../assets/svg/hitzone_logo.svg";
+import GuideIcon from "../../assets/webp/guide_header.webp";
+import NotificationIcon from "../../assets/webp/notification.webp";
+
 const Header = () => {
-    return (
-        <div>
-        </div>
-    )
-}
+  const router = useRouter();
 
+  // 로고 클릭 시 Main 페이지로 이동
+  const handleLogoClick = () => {
+    if (router.pathname !== "/main") {
+      router.push("/main");
+    }
+  };
 
-export default Header
+  return (
+    <div className="flex gap-[156px] p-4 border-b border-grayscale-10 w-full">
+      <div className="flex items-center">
+        <Image 
+          src={HitzoneLogo} 
+          alt="Hitzone" 
+          width={154} 
+          height={27} 
+          onClick={handleLogoClick}
+          className="cursor-pointer"
+        />
+      </div>
+      <div className="flex space-x-3 ml-auto">
+        <Image src={GuideIcon} alt="Guide Icon" width={28} height={20} />
+        <Image src={NotificationIcon} alt="Notification Icon" width={28} height={20} />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
