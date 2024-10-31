947ec9a518d089ad68e3b2625283deeaf289463b
inaemon <github32377@gmail.com>
2024-10-31 10:03:59 +0900

    (#4) 🔄 refactor: 아이콘명 카멜케이스로 변경

--- src/pages/Onboarding/components/BackBar.tsx
+++ src/pages/Onboarding/components/BackBar.tsx
@@ -1,11 +1,11 @@
 import Image from 'next/image';
 
-import back_left_button_gray_icon from '../../../assets/webp/back_left_button_gray.webp';
+import backLeftButtonGrayIcon from '../../../assets/webp/back_left_button_gray.webp';
 
 const BackBar = () => {
     return (
         <div className="px-[15px]">
-            <Image src={back_left_button_gray_icon} alt="뒤로가기 버튼" width={24} height={24} />
+            <Image src={backLeftButtonGrayIcon} alt="뒤로가기 버튼" width={24} height={24} />
         </div>
     )
 }
