7fbf759e27263d71b38d0fccbad8939cec79139e
inaemon <github32377@gmail.com>
2024-11-06 19:26:51 +0900

    (#22) 🎨 design: 메인홈 버튼 고정

--- src/pages/Main/components/SeatRecommendButton.tsx
+++ src/pages/Main/components/SeatRecommendButton.tsx
@@ -20,14 +20,21 @@ const FindSectionButton: React.FC<FindSectionButtonProps> = ({ stadium }) => {
   };
 
   return (
-    <div className="flex justify-center my-5">
-      <button
-        onClick={handleClick}
-        className="px-6 py-3 w-full bg-main-50 text-grayscale-0 text-lg font-bold rounded-lg shadow-md hover:bg-main-60"
-      >
-        나에게 맞는 구역 찾으러 가기
-      </button>
-    </div>
+      <div className="absolute bottom-0 left-0 w-full px-[16px]">
+        {/** 상대 위치로 배트 이미지를 시작하기 버튼으로부터 40px 위에, 왼쪽으로부터 13% 위치에 배치 */}
+        {/** 상대 위치로 공 이미지를 시작하기 버튼으로부터 40px 위에, 왼쪽으로부터 85% 위치에 배치 */}
+        <div className="relative">
+            {/**z-10 relative:  맨 위에 배치 */}
+            <div className="flex justify-center items-center border border-0 rounded-[8px] h-[48px] mb-[120px]
+                            bg-main-50 text-grayscale-0 text-lg font-bold hadow-md hover:bg-main-60 z-0">
+              <button
+                onClick={handleClick}
+              >
+                나에게 맞는 구역 찾으러 가기
+              </button>
+            </div>
+        </div>
+      </div>
   );
 };
 
