138c72e24a08aa96052a9fb9c23f536c88417226
2unhi <ironhak1106@gmail.com>
2024-11-10 20:58:49 +0900

    (#36) 🚀 feat : 공통 레이아웃 BackLogoBar 뒤로가기 버튼 기능 구현

--- src/components/layout/BackLogoBar.tsx
+++ src/components/layout/BackLogoBar.tsx
@@ -1,20 +1,30 @@
+import React from 'react';
 import Image from 'next/image';
+import { useRouter } from 'next/router';
 
 import logoIcon from '../../assets/svg/hitzone_logo.svg';
 import backLeftButtonGrayIcon from '../../assets/webp/back_left_button_gray.webp';
 
 const BackBar = () => {
+    const router = useRouter();
+
     return (
-        <div className="flex py-[14px]">
+        <div className="flex items-center justify-between py-[14px] px-4 bg-white z-20">
             <div className="flex justify-start">
-                <Image src={backLeftButtonGrayIcon} alt="뒤로가기 버튼" width={24} height={24} />
+                <Image
+                    src={backLeftButtonGrayIcon}
+                    alt="뒤로가기 버튼"
+                    width={24}
+                    height={24}
+                    onClick={() => router.back()}
+                    className="cursor-pointer"
+                />
             </div>
-            <div className="flex justify-center w-full">
+            <div className="flex justify-center flex-grow">
                 <Image src={logoIcon} alt="Logo" width={154} height={27} />
             </div>
         </div>
-    )
-}
+    );
+};
 
-
-export default BackBar
+export default BackBar;
