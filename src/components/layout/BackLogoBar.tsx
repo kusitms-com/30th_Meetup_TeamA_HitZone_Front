0c5a2243e092af97d4e41e9c8491135d95eb7a2d
inaemon <github32377@gmail.com>
2024-11-12 06:29:04 +0900

    (#41) 🚀 feat: 로고가 있는 모든 헤더바 로고 클릭시 메인홈 이동 이벤트 추가

--- src/components/layout/BackLogoBar.tsx
+++ src/components/layout/BackLogoBar.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'next/link';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
 
@@ -21,7 +22,9 @@ const BackBar = () => {
                 />
             </div>
             <div className="flex justify-center flex-grow">
-                <Image src={logoIcon} alt="Logo" width={154} height={27} className="cursor-pointer"/>
+                <Link href="/">
+                    <Image src={logoIcon} alt="Logo" width={154} height={27} className="cursor-pointer"/>
+                </Link>
             </div>
         </div>
     );
