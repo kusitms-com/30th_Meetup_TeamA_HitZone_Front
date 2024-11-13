047bd387eeb436e61f3beb07a9e7c26a1417fb15
2unhi <ironhak1106@gmail.com>
2024-11-13 12:15:15 +0900

    (#45) 🎨 design: 챗봇 플로팅 버튼, 루키 프로필 이미지 수정

--- src/components/button/FloatingChatbotButton.tsx
+++ src/components/button/FloatingChatbotButton.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useRouter } from "next/router";
 import Image from "next/image";
-import chatbotIcon from "../../assets/svg/chatbot_button.svg";
+import chatbotIcon from "../../assets/svg/chatbot_floating_button.svg";
 
 const FloatingChatbotButton = () => {
   const router = useRouter();
@@ -13,10 +13,15 @@ const FloatingChatbotButton = () => {
   return (
     <button
       onClick={handleClick}
-      className="fixed bottom-[78px] right-4 md:right-[calc(50%-250px)] flex items-center justify-center transition ease-in-out"
+      className="fixed bottom-[78px] right-4 md:right-[calc(50%-250px)] flex items-center justify-center transition ease-in-out bg-main-10 rounded-full w-12 h-12 shadow-2xl"
       aria-label="히트존 챗봇"
     >
-      <Image src={chatbotIcon} alt="히트존 챗봇" width={48} height={48} />
+      <Image 
+        src={chatbotIcon} 
+        alt="히트존 챗봇" 
+        width={24} 
+        height={24}
+      />
     </button>
   );
 };
