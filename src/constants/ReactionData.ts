9aa9235a42b14894844b6c8ee29925fddc3a1699
inaemon <github32377@gmail.com>
2024-11-17 18:50:22 +0900

    (#51) 🐞 fix: 메인홈 야구장 컴포넌트 일반 폰 기종에서 좌석 인식율을 높이기 위해 이미지 크기 더 작게 조정

--- src/constants/ReactionData.ts
+++ src/constants/ReactionData.ts
@@ -17,13 +17,13 @@ export interface ImageSize {
 export const ImageDimensions: Record<StadiumType, Record<ScreenWidthSize, ImageSize>> = {
     [StadiumType.JAMSIL]: {
         [ScreenWidthSize.SMALL]: { width: 337, height: 319 },
-        [ScreenWidthSize.MEDIUM]: { width: 350, height: 331 },
+        [ScreenWidthSize.MEDIUM]: { width: 347, height: 329 },
         [ScreenWidthSize.LARGE]: { width: 376, height: 356 },
         [ScreenWidthSize.EXTRA_LARGE]: { width: 476, height: 451 },
     },
     [StadiumType.SUWON_KT]: {
         [ScreenWidthSize.SMALL]: { width: 316, height: 345 },
-        [ScreenWidthSize.MEDIUM]: { width: 343, height: 374 },
+        [ScreenWidthSize.MEDIUM]: { width: 337, height: 368 },
         [ScreenWidthSize.LARGE]: { width: 366, height: 400 },
         [ScreenWidthSize.EXTRA_LARGE]: { width: 467, height: 510 },
     },
