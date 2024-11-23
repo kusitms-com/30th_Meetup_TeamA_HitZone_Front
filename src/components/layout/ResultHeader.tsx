efdaf077991a4cb43454ac40a57a22bc9cda0216
2unhi <ironhak1106@gmail.com>
2024-11-24 01:55:37 +0900

    (#68) 🎨 design: 구역 추천 결과 페이지 색상 수정

--- src/components/layout/ResultHeader.tsx
+++ src/components/layout/ResultHeader.tsx
@@ -13,7 +13,7 @@ const ResultHeader: React.FC<ResultHeaderProps> = ({
   hashTags,
 }) => {
   return (
-    <div className="flex justify-between w-full border-b border-grayscale-10 p-[15px]">
+    <div className="flex justify-between w-full border-b border-grayscale-10 p-[15px] bg-main-0">
       <div className="flex justify-center items-center w-full">
         <p className="text-lg text-grayscale-90 font-semibold text-center">
           야구장 유형 테스트 결과
