c7592ad462285a6d074704e5224946c9451d4efc
inaemon <github32377@gmail.com>
2024-11-23 15:37:31 +0900

    (#47) 🔄 refactor: 채팅창 루키 프로필 컴포넌트화

--- src/pages/Chatbot/components/ChatbotStart.tsx
+++ src/pages/Chatbot/components/ChatbotStart.tsx
@@ -1,20 +1,12 @@
 import React from "react";
-import Image from "next/image";
-import chatbotIcon from "@/src/assets/svg/chatbot_lookie.svg";
 
+import RookieProfile from "./RookieProfile";
 import RookieMessageTail from "./RookieMessageTail";
 
 const ChatbotStart = () => {
   return (
     <div className="flex items-start mb-2 transition-opacity duration-1000">
-      <div className="flex items-center justify-center bg-main-40 rounded-md w-11 h-11 mr-2">
-        <Image 
-          src={chatbotIcon} 
-          alt="챗봇 아이콘" 
-          width={42} 
-          height={41}
-        />
-      </div>
+      <RookieProfile/>
 
       <div className="px-2">
         <p className="text-[14px] font-medium text-black mb-[6px]">챗봇 루키</p>
