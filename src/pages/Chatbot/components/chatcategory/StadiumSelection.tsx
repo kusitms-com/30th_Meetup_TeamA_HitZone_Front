c61509b794f7a28215d82ad843ed590788071020
inaemon <github32377@gmail.com>
2024-11-24 04:02:39 +0900

    (#47) 🚀 feat: 챗봇 스타디움 선택 카테고리 제공하지 않는 구장은 이벤트 비활성화

--- src/pages/Chatbot/components/chatcategory/StadiumSelection.tsx
+++ src/pages/Chatbot/components/chatcategory/StadiumSelection.tsx
@@ -10,23 +10,34 @@ interface StadiumSelectionProps {
 }
 
 const StadiumSelection = ({ stadiums, onSelect }: StadiumSelectionProps) => {
+  
+  // 카테고리에서 스타디움 리스트를 렌더링하는 함수
+  const renderStadium = (stadium: string) => {
+    const selectable = stadium === StadiumType.JAMSIL || stadium === StadiumType.SUWON_KT;
+
+    return (
+      <div
+        key={stadium}
+        onClick={selectable ? () => onSelect(stadium) : undefined}
+        className={`bg-grayscale-5 py-2 rounded-md text-center
+          ${
+            stadium !== StadiumType.JAMSIL && stadium !== StadiumType.SUWON_KT ? "text-grayscale-30" : "text-grayscale-90 cursor-pointer hover:bg-grayscale-10"
+          }`
+        }>
+        {stadium}
+      </div>
+    )
+  }
+  
   return (
     <div className="flex">
       <div className="bg-white rounded-lg max-w-[245px] w-full">
         <Image src={stadiumIcon} alt="구장 선택" width={245} height={99} />
         <div className="grid grid-cols-1 gap-2 p-3 text-xs font-regular">
           {stadiums.map((stadium) => (
-            <div
-              key={stadium}
-              onClick={() => onSelect(stadium)}
-              className={`bg-grayscale-5 py-2 rounded-md text-center
-                ${
-                  stadium !== StadiumType.JAMSIL && stadium !== StadiumType.SUWON_KT ? "text-grayscale-30" : "text-grayscale-90 cursor-pointer hover:bg-grayscale-10"
-                }`
-              }>
-              {stadium}
-            </div>
-          ))}
+            renderStadium(stadium)
+            )
+          )}
         </div>
       </div>
     </div>
