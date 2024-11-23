d869a6483c6c762ac2de8c1679ed1734eb5b8b66
inaemon <github32377@gmail.com>
2024-11-23 15:43:31 +0900

    (#47) 🔄 refactor: 메시지 말풍선 컴포넌트명 수정

--- src/pages/Chatbot/components/ChatbotStart.tsx
+++ src/pages/Chatbot/components/ChatbotStart.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import RookieProfile from "./RookieProfile";
-import RookieMessageTail from "./RookieMessageTail";
+import RookieMessageWithTail from "./RookieMessageWithTail";
 
 const ChatbotStart = () => {
   return (
@@ -10,7 +10,7 @@ const ChatbotStart = () => {
 
       <div className="px-2">
         <p className="text-[14px] font-medium text-black mb-[6px]">챗봇 루키</p>
-        <RookieMessageTail messageList={["안녕하세요! 챗봇 루키 입니다.", "궁금하신 정보를 실시간 채팅으로 확인하실 수 있습니다."]}/>
+        <RookieMessageWithTail messageList={["안녕하세요! 챗봇 루키 입니다.", "궁금하신 정보를 실시간 채팅으로 확인하실 수 있습니다."]}/>
       </div>
     </div>
   );
