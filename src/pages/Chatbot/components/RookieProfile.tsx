a72cf365b0521173eee23d9a7ae187239d8ecac7
inaemon <github32377@gmail.com>
2024-11-24 01:54:13 +0900

    (#47) 🎨 design: 챗봇 프로필 정사각형 크기 변경

--- src/pages/Chatbot/components/RookieProfile.tsx
+++ src/pages/Chatbot/components/RookieProfile.tsx
@@ -5,7 +5,7 @@ import chatbotIcon from "@/src/assets/svg/chatbot_profile.svg"; // 얘는 webp�
 /** 꼬랑지가 붙은 버젼 */
 const RookieProfile = () => {
   return (
-    <div className="flex items-center justify-center bg-main-40 rounded-lg ">
+    <div className="flex items-center justify-center bg-main-40 rounded-lg w-11 h-11">
         <Image 
           src={chatbotIcon} 
           alt="챗봇 아이콘" 
