cf927fdbdc2acff7b1504f54cb115a6f774ee9eb
inaemon <github32377@gmail.com>
2024-11-24 00:12:04 +0900

    (#47) 🎨 design: 챗봇 FAQ 카테고리바 호버 색상과 커서 포인트 추가 및 디자인 수정

--- src/pages/Chatbot/components/FAQCategoryBar.tsx
+++ src/pages/Chatbot/components/FAQCategoryBar.tsx
@@ -10,20 +10,22 @@ const FAQCategoryBar = ({ isVisible }: FAQCategoryBarProps) => {
   
   return (
     <div
-      className={`fixed bottom-[60px] left-1/2 transform -translate-x-1/2 max-w-[500px] w-full py-3 px-5 bg-grayscale-5 rounded-t-lg transition-transform duration-300 ${
+      className={`fixed bottom-[60px] left-1/2 transform -translate-x-1/2 max-w-[500px] w-full py-4 bg-grayscale-5 rounded-t-2xl transition-transform ${
         isVisible ? "translate-y-0 opacity-100" : "translate-y-full opacity-0"
       }`}
     >
-      <div className="flex flex-col space-y-3">
+      <div className="flex flex-col ">
         {Object.keys(categories).map((key) => {
             const category = categories[key as keyof typeof categories];
             return (
-              <p
-                key={key}
-                className="text-sm flex items-center text-grayscale-90 font-regular"
-              >
-                {category.frontendValue}
-              </p>
+              <div className="cursor-pointer hover:bg-grayscale-10 px-5 py-1">
+                <p
+                  key={key}
+                  className="text-sm flex items-center text-grayscale-90 font-regular "
+                >
+                  {category.frontendValue}
+                </p>
+              </div>
             );
         })}
       </div>
