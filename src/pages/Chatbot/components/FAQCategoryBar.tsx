c40ad1dd8d9c252d43e594d57257e6c67f3046ea
inaemon <github32377@gmail.com>
2024-11-24 01:08:13 +0900

    (#47) 🎨 design: 챗봇 FAQ 카테고리바에 닫는 핑크바 버튼 추가 및 디자인 수정

--- src/pages/Chatbot/components/FAQCategoryBar.tsx
+++ src/pages/Chatbot/components/FAQCategoryBar.tsx
@@ -5,15 +5,20 @@ const FAQCategoryBar = () => {
   const { baseballCategories, questionCategories: categories } = questionCategories;
   
   return (
-    <div className="fixed bottom-[60px] left-1/2 transform -translate-x-1/2 max-w-[500px] w-full py-4 bg-grayscale-5 rounded-t-2xl transition-transform">
+    <div className="fixed bottom-[60px] left-1/2 transform -translate-x-1/2 max-w-[500px] w-full bg-grayscale-5 rounded-t-2xl transition-transform pb-2">
+
+          {/** 상태 바 */}
+      <div className="flex justify-center text-center items-center pt-3 py-3 cursor-pointer hover:bg-grayscale-10">
+        <div className="w-9 h-[2px] bg-main-40 rounded-full"/>
+      </div>
       <div className="flex flex-col ">
         {Object.keys(categories).map((key) => {
             const category = categories[key as keyof typeof categories];
             return (
-              <div className="cursor-pointer hover:bg-grayscale-10 px-5 py-1">
+              <div className="cursor-pointer px-5 py-1.5 hover:bg-grayscale-10">
                 <p
                   key={key}
-                  className="text-sm flex items-center text-grayscale-90 font-regular "
+                  className="text-sm flex text-grayscale-90 font-regular "
                 >
                   {category.frontendValue}
                 </p>
