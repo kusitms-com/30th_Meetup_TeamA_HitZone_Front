958c2a0928a637e1f3b9eca059a3002f86ad9815
inaemon <github32377@gmail.com>
2024-11-28 03:37:53 +0900

    (#79) 🎨 design: 채팅 로딩시 로딩 이미지 아래에 설명 문구 추가

--- src/pages/Chatbot/components/loading/ChatbotClovaLoading.tsx
+++ src/pages/Chatbot/components/loading/ChatbotClovaLoading.tsx
@@ -3,10 +3,11 @@ import chatbotLoadingAnimation from "@/src/assets/gif/chatbot_loading_baseball_p
 
 const ChatbotClovaLoading = () => {
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
+    <div className="flex-col fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
         <div className="animate-spin">
             <Image src={chatbotLoadingAnimation} alt="Loading" className="w-24 h-24" />
         </div>
+        <p className="font-semibold text-grayscale-0">루키가 생각중이에요..</p>
     </div>
   );
 };
